3c220c1646a3a170776caa1aed317d97f1d1460f
Đỗ Vũ Tuấn <tuandv02@ominext.com>
2020-05-26 15:16:30 +0700

    update project

---
+++ resources/views/mail/mail_bill_customer.blade.php
@@ -0,0 +1,416 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+</head>
+<style type="text/css">
+    body {
+        padding: 0 10px;
+        background-color: #f2f2f2;
+        margin: 0px auto;
+    }
+
+    .container-background-fff {
+        background: #fff;
+    }
+
+    .container-background-fff-height {
+        min-height: 100px;
+    }
+
+    .container {
+        width: 100%;
+        padding-right: 15px;
+        padding-left: 15px;
+        margin-right: auto;
+        margin-left: auto
+    }
+
+    @media (min-width: 576px) {
+        .container {
+            max-width: 540px
+        }
+    }
+
+    @media (min-width: 768px) {
+        .container {
+            max-width: 720px
+        }
+    }
+
+    @media (min-width: 992px) {
+        .container {
+            max-width: 960px
+        }
+    }
+
+    @media (min-width: 1200px) {
+        .container {
+            max-width: 1140px
+        }
+    }
+
+    .table {
+        font-size: 0px;
+        width: 100%;
+    }
+
+    .div-margin {
+        margin: 0px auto;
+    }
+
+    .table-tr {
+        text-align: center;
+        vertical-align: top;
+        direction: ltr;
+        font-size: 0px;
+        padding: 40px 0px 30px;
+    }
+
+    .table-tr-div {
+        vertical-align: middle;
+        display: inline-block;
+        direction: ltr;
+        font-size: 13px;
+        text-align: left;
+        width: 100%;
+    }
+
+    .table-tr-td {
+        word-wrap: break-word;
+        font-size: 0px;
+        padding: 0px;
+    }
+
+    .table-tr-td-div {
+        color: #4a4a4a;
+        font-family: 'Open Sans', Helvetica, 'Hiragino Kaku Gothic ProN', Meiryo, Arial, sans-serif;
+        font-size: inherit;
+        line-height: inherit;
+        text-align: center;
+    }
+
+    .table-tr-td-div-div {
+        padding-right: 25px;
+        padding-left: 25px;
+    }
+
+    .table-two {
+        font-size: 0px;
+        width: 100%;
+        background: #4cbd9b;
+    }
+
+    .table-two-tr-td {
+        text-align: center;
+        vertical-align: top;
+        direction: ltr;
+        font-size: 0px;
+        padding: 28px 15px;
+    }
+
+    .table-two-tr-td-div {
+        vertical-align: top;
+        display: inline-block;
+        direction: ltr;
+        font-size: 13px;
+        text-align: left;
+        width: 100%;
+    }
+
+    .table-two-tr-td-div-p {
+        font-size: 21px;
+        font-weight: 400;
+        margin-top: 3px;
+        margin-bottom: 0;
+    }
+
+    .table-three {
+        font-size: 0px;
+        width: 100%;
+        background: #fff;
+    }
+
+    .table-three-tr-td {
+        text-align: center;
+        vertical-align: top;
+        direction: ltr;
+        font-size: 0px;
+        padding: 10px 15px;
+    }
+
+    .table-three-tr-td-div {
+        vertical-align: top;
+        display: inline-block;
+        direction: ltr;
+        font-size: 13px;
+        text-align: left;
+        width: 100%;
+    }
+
+    .table-three-tr-td-tr-td {
+        word-wrap: break-word;
+        font-size: 0px;
+        padding: 10px 0px;
+    }
+
+    .table-three-tr-td-tr-td-div {
+        color: #4a4a4a;
+        font-family: 'Open Sans', Helvetica, 'Hiragino Kaku Gothic ProN', Meiryo, Arial, sans-serif;
+        font-size: 16px;
+        line-height: 30px;
+        text-align: left;
+    }
+
+    .table-four {
+        border-collapse: collapse;
+        width: 100%;
+        font-size: 16px;
+        line-height: 22px;
+        color: #4a4a4a;
+        border-bottom: solid 1px #eee;
+    }
+
+    .table-four-th {
+        width: 25%;
+        font-weight: 400;
+        vertical-align: top;
+        border-top: solid 1px #eee;
+        border-left: solid 1px #eee;
+        padding-top: 12px;
+        padding-left: 20px;
+        padding-right: 15px;
+        padding-bottom: 12px;
+    }
+
+    .table-four-td {
+        font-weight: 400;
+        border-top: solid 1px #eee;
+        border-left: solid 1px #eee;
+        border-right: solid 1px #eee;
+        padding-top: 4px;
+        padding-left: 15px;
+        padding-right: 20px;
+        padding-bottom: 4px;
+    }
+
+    .table-four-td-p {
+        margin-top: 8px;
+        margin-bottom: 6px;
+    }
+
+    .table-four-td-span {
+        padding-left: 1em;
+        padding-right: 1em;
+        color: #666;
+    }
+
+    .table-five {
+        font-size: 0px;
+        width: 100%;
+        min-height: 100px;
+        background: #fff;
+    }
+
+    .table-five-tr-td {
+        text-align: center;
+        vertical-align: top;
+        direction: ltr;
+        font-size: 0px;
+        padding: 5px 20px;
+        padding-top: 20px;
+    }
+
+    .table-five-tr-td-div {
+        vertical-align: top;
+        display: inline-block;
+        direction: ltr;
+        font-size: 13px;
+        text-align: left;
+        width: 100%;
+    }
+
+    .table-five-tr-td-div-tr-td-div {
+        color: #74758b;
+        font-family: 'Open Sans', Helvetica, 'Hiragino Kaku Gothic ProN', Meiryo, Arial, sans-serif;
+        font-size: 12px;
+        font-weight: 400;
+        line-height: 20px;
+        text-align: center;
+    }
+</style>
+<body>
+<div class="container">
+    <table class="table" cellpadding="0" cellspacing="0" border="0">
+        <tbody>
+        <tr>
+            <td>
+                <div class="div-margin">
+                    <table class="table" cellpadding="0" cellspacing="0" align="center" border="0">
+                        <tbody>
+                        <tr>
+                            <td class="table-tr">
+                                <div class="m_-1596198941565798281mj-column-per-100 table-tr-div">
+                                    <table role="presentation" cellpadding="0" cellspacing="0"
+                                           style="vertical-align: middle;" width="100%" border="0">
+                                        <tbody>
+                                        <tr>
+                                            <td class="table-tr-td" align="center">
+                                                <div class="table-tr-td-div"><a href="{{env('APP_URL_PROJECT')}}">
+                                                        <img width="158"
+                                                             src="https://giaysneakerhcm.com/wp-content/uploads/2018/06/ruby-store.png"/>
+                                                    </a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+    <div style="background: #4cbd9b;">
+        <table class="table-two" cellpadding="0" cellspacing="0" align="center" border="0">
+            <tbody>
+            <tr>
+                <td class="table-two-tr-td">
+                    <div class="m_-1596198941565798281mj-column-per-100 table-two-tr-td-div">
+                        <table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                            <tbody>
+                            <tr>
+                                <td class="table-tr-td" align="center">
+                                    <div class="table-tr-td-div">
+                                        <div class="m_-1596198941565798281contents-section-block table-tr-td-div-div">
+                                            <p class="table-two-tr-td-div-p">{{\Carbon\Carbon::now()->format('d-m-Y')}}</p>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="container-background-fff">
+        <table class="table-three" cellpadding="0" cellspacing="0" align="center" border="0">
+            <tbody>
+            <tr>
+                <td class="table-three-tr-td">
+                    <div class="m_-1596198941565798281mj-column-per-100 table-three-tr-td-div">
+                        <table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                            <tbody>
+                            <tr>
+                                <td class="table-three-tr-td-tr-td" align="left">
+                                    <div class="table-three-tr-td-tr-td-div">
+                                        <div class="m_-1596198941565798281contents-section-block table-tr-td-div-div">
+                                            <b style="font-weight: 600;">Over Due</b> 2 issues<br/>
+                                            The following issues are overdue:
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="table-three-tr-td-tr-td" align="left">
+                                    <div class="table-three-tr-td-tr-td-div">
+                                        <div class="m_-1596198941565798281contents-section-block table-tr-td-div-div">
+                                            <table border="0" cellpadding="0" cellspacing="0" width="100%"
+                                                   class="m_-1596198941565798281table-striped-list table-four">
+                                                <tbody>
+                                                <tr>
+                                                    <th class="m_-1596198941565798281table-striped-list__th table-four-th"
+                                                        valign="top">
+                                                        <a href="https://ominext.backlog.com/view/PFM-2"
+                                                           style="color: #4cbd9b; text-decoration: none;"
+                                                           target="_blank"
+                                                           data-saferedirecturl="https://www.google.com/url?q=https://ominext.backlog.com/view/PFM-2&amp;source=gmail&amp;ust=1590561967217000&amp;usg=AFQjCNF9IRSQuJP9KqVXCafkeioWfEZ5EQ">
+                                                            PFM-2</a>
+                                                    </th>
+                                                    <td class="m_-1596198941565798281table-striped-list__td table-four-td">
+                                                        <p class="table-four-td-p">Thiết kế màn
+                                                            hình quản lý Client</p>
+                                                        <p class="table-four-td-p">
+                                                            <span style="white-space: nowrap;">Status: Resolved</span>
+                                                            <span class="table-four-td-span">|</span>
+                                                            <span>Assignee: NghiemVanNam[namnv04](dev)</span>
+                                                        </p>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th class="m_-1596198941565798281table-striped-list__th table-four-th"
+                                                        valign="top" bgcolor="#eee">
+                                                        <a href="https://ominext.backlog.com/view/PFM-1"
+                                                           style="color: #4cbd9b; text-decoration: none;"
+                                                           target="_blank"
+                                                           data-saferedirecturl="https://www.google.com/url?q=https://ominext.backlog.com/view/PFM-1&amp;source=gmail&amp;ust=1590561967217000&amp;usg=AFQjCNF-wSnaQMpDr0LEkrZupaORTrvMTg">PFM-1</a>
+                                                    </th>
+                                                    <td class="m_-1596198941565798281table-striped-list__td table-four-td"
+                                                        bgcolor="#eee">
+                                                        <p class="table-four-td-p">Thiết kế màn
+                                                            hình master services</p>
+                                                        <p class="table-four-td-p">
+                                                            <span style="white-space: nowrap;">Status: Resolved</span>
+                                                            <span class="table-four-td-span">|</span>
+                                                            <span>Assignee: Nguyễn Thế Đại[Daint02](dev)</span>
+                                                        </p>
+                                                    </td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="container-background-fff-height">
+        <table class="table-five" cellpadding="0" cellspacing="0" align="center" border="0">
+            <tbody>
+            <tr>
+                <td class="table-five-tr-td">
+                    <div class="m_-1596198941565798281mj-column-per-100 table-five-tr-td-div">
+                        <table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                            <tbody>
+                            <tr>
+                                <td class="table-tr-td" align="center">
+                                    <div class="table-five-tr-td-div-tr-td-div">
+                                        You received this email because there is a new Project Report.<br/>
+                                        To change your email preferences, please go to your
+                                        <a
+                                            href="https://ominext.backlog.com/settings/mail/PFM"
+                                            style="color: #74758b; text-decoration: underline;"
+                                            target="_blank"
+                                            data-saferedirecturl="https://www.google.com/url?q=https://ominext.backlog.com/settings/mail/PFM&amp;source=gmail&amp;ust=1590561967217000&amp;usg=AFQjCNHjbjEw7Pw2N3CYG4Y6zCqG-FmvcQ"
+                                        >
+                                            Email Settings
+                                        </a>
+                                        .
+                                    </div>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+</body>
+</html>
