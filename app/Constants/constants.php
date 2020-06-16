8b550128e98e4e0db7b43cf68ed7f8369ad2552b
Đỗ Vũ Tuấn <tuandv02@ominext.com>
2020-06-16 09:39:52 +0700

    update

--- app/Constants/constants.php
+++ app/Constants/constants.php
@@ -9,6 +9,7 @@ define('FOUR', 4);
 define('FIVE', 5);
 define('SIX', 6);
 define('EIGHT', 8);
+define('TEN', 10);
 define('TWELVE', 12);
 define('PAGINATE', 30);
 define('ONE_HUNDRED', 100);
@@ -16,7 +17,9 @@ define('TEN_THOUSAND', 10000);
 define('ONE_HUNDRED_THOUSAND', 100000);
 define('ONE_MILLION', 1000000);
 define('CURRENCY', 23295.03);
+
 define('USERS', 'users');
 define('CATEGORIES', 'categories');
 define('PRODUCTS', 'products');
+
 define('TYPE_FILES', '.png,.jpg, .gif, .jpeg');
